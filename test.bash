97dd9aceda8c5f2ed2a206e1b979d425303474d1
rudyde <33863297+rudyde@users.noreply.github.com>
2017-11-22 19:36:19 +0100

    Revert "Branch3"

--- test.bash
+++ test.bash
@@ -4,8 +4,3 @@ echo "change 1"
 echo "change 2"
 #######################
 echo "Branch 1 is added"
-######################
-echo "Branch 3 is added"
-########################
-echo "Branch 1 is merged to branch 3 by pull request"
-
